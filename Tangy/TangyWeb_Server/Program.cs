a7f12c93a7f293f9460163cd937644d6cd4d9d76
DeeJayJay <deejayjay@zoho.com>
2023-12-23 15:20:27 -0700

    Adds Business, Common and DataAccess projects, and adds the DbContext. Added the first migration.

--- Tangy/TangyWeb_Server/Program.cs
+++ Tangy/TangyWeb_Server/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Tangy_DataAccess.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
